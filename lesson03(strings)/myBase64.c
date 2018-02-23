04686180ea33a77df3a00f03595466b4ca5399e0
Pavels Ivanovs <32202454+PAVELMANS@users.noreply.github.com>
2018-02-23 16:44:14 +0200

    lala

--- lesson03(strings)/myBase64.c
+++ lesson03(strings)/myBase64.c
@@ -6,13 +6,20 @@ void encode(FILE *f_in, FILE *f_out)
 {
     int ch[3];
     char code[4];
-    for (int i = 0; i < 3; i++) ch[i] = getc(f_in);
-    code[0] = ch[0] >> 2;
-    code[1] = ((ch[0] & 0x03) << 4) | (ch[1] >> 4);
-    code[2] = ((ch[1] & 0x0F) << 2) | (ch[2] >> 6);
-    code[3] = ch[2] & 0x3F;
-    char encripted[3];
-    for (int i = 0; i < 3; i++) fprintf(f_out, "%c", code[i]);
+    do {
+        for (int i = 0; i < 3; i++) {
+          ch[i] = getc(f_in);
+          if (ch[i] == EOF) {
+                return;
+          }
+        }
+        code[0] = ch[0] >> 2;
+        code[1] = ((ch[0] & 0x03) << 4) | (ch[1] >> 4);
+        code[2] = ((ch[1] & 0x0F) << 2) | (ch[2] >> 6);
+        code[3] = ch[2] & 0x3F;
+        char encripted[3];
+        for (int i = 0; i < 4; i++) fprintf(f_out, "%c", base_ch[code[i]]);
+    } while (1);
 }
 
 void decode(FILE *f_in, FILE *f_out)
@@ -22,7 +29,9 @@ void decode(FILE *f_in, FILE *f_out)
 
 int main(int argc, char *argv[])
 {
-    char mode = *argv[0];
+    char mode = *argv[1];
+    //char inFil[] = *argv[2];
+    //char outFil[] = *argv[3];
     FILE *inFile = fopen(argv[2], "rb");
     FILE *outFile = fopen(argv[3], "wb");
 
